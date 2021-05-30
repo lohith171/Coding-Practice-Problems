217fd4c7d55cecefbe140b327dae820330933674
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-05-30 17:31:38 +0530

    Update Maximum Subarray Sum.cpp

--- Arrays/Maximum Subarray Sum.cpp
+++ Arrays/Maximum Subarray Sum.cpp
@@ -1,1 +1,8 @@
-kdkldgnkl
+int Solution::maxSubArray(const vector<int> &A) {
+    int best=INT_MIN,sum=0;
+    for(int i=0;i<A.size();i++){
+        sum=max(A[i],sum+A[i]);
+        best=max(best,sum);
+    }
+    return best;
+}

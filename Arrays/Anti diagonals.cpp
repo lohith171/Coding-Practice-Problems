560610b06c1eaa9ee6eba325e4d2abd16a9f9d83
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-05-31 22:41:13 +0530

    Create Anti diagonals.cpp

---
+++ Arrays/Anti diagonals.cpp
@@ -0,0 +1,30 @@
+vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
+    int n=A.size();
+    if(n==1)return A;
+    vector<vector<int>>res;
+    res.push_back({A[0][0]});
+    int i=0,j=1;
+    bool flag=false;
+    while(true){
+        int i1=i,j1=j;
+        vector<int>temp;
+        while(i1<n && j1>=0){
+            temp.push_back(A[i1][j1]);
+            i1++;
+            j1--;
+        }
+        res.push_back(temp);
+        if(flag==false)j++;
+        else i++;
+        if(j==n){
+            flag=true;
+            j--;
+            i++;
+        }
+        if(i==n-1 && j==n-1){
+            res.push_back({A[i][j]});
+            break;
+        }
+    }
+    return res;
+}

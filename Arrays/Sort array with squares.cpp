ee590a0d4a59ae37309d47f04d35806144f292e6
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-05-31 15:05:46 +0530

    Update and rename h to Sort array with squares.cpp

---
+++ Arrays/Sort array with squares.cpp
@@ -0,0 +1,20 @@
+vector<int> Solution::solve(vector<int> &A) {
+    for(int i=0;i<A.size();i++){
+        A[i]*=A[i];
+    }
+    vector<int>res(A.size(),0);
+    int ind=A.size()-1;
+    int i=0,j=A.size()-1;
+    while(i<=j){
+        if(A[i]>A[j]){
+            res[ind]=A[i];
+            i++;
+            ind--;
+        }else{
+            res[ind]=A[j];
+            j--;
+            ind--;
+        }
+    }
+    return res;
+}

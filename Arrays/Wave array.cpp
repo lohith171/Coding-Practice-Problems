f32a18b15c0d54c1cb299c514af9f00376a3630b
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-05-31 16:09:17 +0530

    Create Wave array.cpp

---
+++ Arrays/Wave array.cpp
@@ -0,0 +1,8 @@
+vector<int> Solution::wave(vector<int> &A) {
+    sort(A.begin(),A.end());
+    for(int i=0;i<A.size();i+=2){
+        if(i==A.size()-1)break;
+        swap(A[i],A[i+1]);
+    }
+    return A;
+}

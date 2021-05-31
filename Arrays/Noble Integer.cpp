5aa1f1859674ffc798c51e3f0eab7b0deea13f9f
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-05-31 15:59:34 +0530

    Create Noble Integer.cpp

---
+++ Arrays/Noble Integer.cpp
@@ -0,0 +1,12 @@
+int Solution::solve(vector<int> &A) {
+    sort(A.begin(),A.end());
+    int n=A.size();
+    unordered_map<int,int>m;
+    for(int i=0;i<A.size();i++){
+        m[A[i]]=i+1;
+    }
+    for(int i=0;i<n;i++){
+        if(n-m[A[i]]==A[i])return 1;
+    }
+    return -1;
+}

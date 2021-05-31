09a86129a1aac7f6751a9f2e4b09a8d7ab599144
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-05-31 15:02:13 +0530

    Create Leaders in an array.cpp

---
+++ Arrays/Leaders in an array.cpp
@@ -0,0 +1,11 @@
+vector<int> Solution::solve(vector<int> &A) {
+    vector<int>res;
+    int m=INT_MIN;
+    for(int i=A.size()-1;i>=0;i--){
+        if(A[i]>m){
+            m=A[i];
+            res.push_back(A[i]);
+        }
+    }
+    return res;
+}

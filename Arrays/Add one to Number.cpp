b1e488c08e4ca22382bc81f12c9647b8520bfee8
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-05-31 15:21:29 +0530

    Create Add one to Number.cpp

---
+++ Arrays/Add one to Number.cpp
@@ -0,0 +1,28 @@
+vector<int> Solution::plusOne(vector<int> &A) {
+    int carry=1;
+    int n=A.size();
+    int i=n-1;
+    while(i>=0){
+        int x=(A[i]+carry)%10;
+        carry= (A[i]+carry)/10;
+        A[i]=x;
+        i--;
+    }
+    if(carry!=0){
+        A.insert(A.begin(),carry);
+    }
+    vector<int>res;
+    bool flag=false;
+    for(int i=0;i<A.size();i++){
+        if(A[i]==0 && flag==false){
+            continue;
+        }else if(flag==false && A[i]!=0){
+            res.push_back(A[i]);
+            flag=true;
+        }else{
+            res.push_back(A[i]);
+        }
+    }
+    
+    return res;
+}

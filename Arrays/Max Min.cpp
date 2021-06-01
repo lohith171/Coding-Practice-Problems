7b83320e0f1af04223341134274d2c89862344e2
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-06-01 08:42:26 +0530

    Update Max Min.cpp

--- Arrays/Max Min.cpp
+++ Arrays/Max Min.cpp
@@ -1,3 +1,5 @@
+//https://www.interviewbit.com/problems/max-min-05542f2f-69aa-4253-9cc7-84eb7bf739c4/
+
 int Solution::solve(vector<int> &A) {
     int max= *max_element(A.begin(),A.end());
     int min= *min_element(A.begin(),A.end());

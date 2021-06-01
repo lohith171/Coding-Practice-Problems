a159bc1af65b35064216fe85ddab769261461158
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-06-01 08:43:23 +0530

    Update and rename Maximum Subarray Sum.cpp to Maximum contiguous Subarray Sum.cpp

--- Arrays/Maximum contiguous Subarray Sum.cpp
+++ Arrays/Maximum contiguous Subarray Sum.cpp
@@ -1,3 +1,5 @@
+//https://www.interviewbit.com/problems/max-sum-contiguous-subarray/
+
 int Solution::maxSubArray(const vector<int> &A) {
     int best=INT_MIN,sum=0;
     for(int i=0;i<A.size();i++){

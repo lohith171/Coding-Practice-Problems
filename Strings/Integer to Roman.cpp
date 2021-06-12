133690b0c380763eac4ac8de5618ac81414b8bce
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-06-12 12:12:23 +0530

    Update Integer to Roman.cpp

--- Strings/Integer to Roman.cpp
+++ Strings/Integer to Roman.cpp
@@ -8,5 +8,18 @@ string Solution::intToRoman(int A) {
         string X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         // 1, 2, ... 9
         string I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return M[A/1000] + C[(A%1000)/100] + X[(A%100)/10] + I[A%10];
+        int i=0; 
+        while(A) {
+        int d=A%10;
+    A=A/10;
+    i++;
+    if( i==1) {
+        res= I[d]+res; 
+    } else if( i==2){ 
+      res= X[d]+res; 
+        }else if( i==3){
+            res= C[d]+res;
+        }else res= M[d]+res;
+        
+} return res;
 }

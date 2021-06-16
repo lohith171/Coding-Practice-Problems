8b69131ef1807d74d8eb51edd4ebd6a90b5f914c
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-06-16 11:14:34 +0530

    Update Minimum Path sum in a grid.cpp

--- Dynamic Programming/Minimum Path sum in a grid.cpp
+++ Dynamic Programming/Minimum Path sum in a grid.cpp
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/minimum-path-sum/
 
+//Recursive...
 int m,n;
  int solve(int i,int j,vector<vector<int>>& grid, vector<vector<int>>& dp){
         if(dp[i][j]!= -1)return dp[i][j];
@@ -18,3 +19,24 @@ int m,n;
         int res=solve(0,0,grid,dp);
         return res;
     }
+
+//Iterative...
+
+int Solution::minPathSum(vector<vector<int> > &A) {
+    if(A.size()==0)return 0;
+    int m=A.size(),n=A[0].size();
+    int dp[m][n];
+    dp[0][0]=A[0][0];
+    for(int i=1;i<m;i++){
+        dp[i][0]=dp[i-1][0]+A[i][0];
+    }
+    for(int j=1;j<n;j++){
+        dp[0][j]=dp[0][j-1]+A[0][j];
+    }
+    for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            dp[i][j]=A[i][j]+min(dp[i][j-1],dp[i-1][j]);
+        }
+    }
+    return dp[m-1][n-1];
+}

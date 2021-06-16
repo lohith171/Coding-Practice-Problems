510c2d531568c97b3823de1fb098172b282e016b
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-06-16 19:44:47 +0530

    Update longest valid Parenthesis.cpp

--- Dynamic Programming/longest valid Parenthesis.cpp
+++ Dynamic Programming/longest valid Parenthesis.cpp
@@ -1,34 +1,28 @@
-//https://leetcode.com/problems/longest-valid-parentheses/
+//https://www.interviewbit.com/problems/longest-valid-parentheses/
 
-int longestValidParentheses(string s) {
-         stack<int>st;
-        int n=s.length();
-        if(n==0)return 0;
-        vector<bool>v(n,true);
-        for(int i=0;i<n;i++){
-            if(s[i]=='('){
-                st.push(i);
-            }else{
-                if(!st.empty()){
-                    st.pop();
-                }else{
-                    v[i]=false;
-                }
-            }
+int Solution::longestValidParentheses(string A) {
+    int n=A.size();
+    vector<bool>v(n,1);
+    stack<int>st;
+    for(int i=0;i<n;i++){
+        if(A[i]=='(')st.push(i);
+        else{
+            if(!st.empty())st.pop();
+            else v[i]=false;
         }
-        while(!st.empty()){
-            v[st.top()]=false;
-            st.pop();
-        }
-        int temp=-1;
-        int m=INT_MIN;
-        for(int i=0;i<n;i++){
-            if(v[i]==false){
-                m=max(m,i-temp-1);
-                temp=i;
-            }
-        }
-        if(temp==-1)return n;
-        m=max(m,n-temp-1);
-        return m;
     }
+    while(!st.empty()){
+        v[st.top()]=false;
+        st.pop();
+    }
+    int temp=-1;
+    int res=0;
+    for(int i=0;i<n;i++){
+        if(v[i]==false){
+            res=max(res,i-temp-1);
+            temp=i;
+        }
+    }
+    res=max(res,n-temp-1);
+    return res;
+}

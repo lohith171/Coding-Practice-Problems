0ab38092912a21db8554799340177d37d1148015
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-06-19 13:29:18 +0530

    Update Paint House.cpp

--- Dynamic Programming/Paint House.cpp
+++ Dynamic Programming/Paint House.cpp
@@ -7,16 +7,11 @@ int Solution::solve(vector<vector<int> > &A) {
     dp[0][0]=A[0][0];
     dp[0][1]=A[0][1];
     dp[0][2]=A[0][2];
-    for(int i=1;i<n;i++){
-        for(int j=0;j<3;j++){
-            if(j==0){
-                dp[i][j]=A[i][j]+min(dp[i-1][1],dp[i-1][2]);
-            }else if(j==1){
-                dp[i][j]=A[i][j]+min(dp[i-1][0],dp[i-1][2]);
-            }else{
-                 dp[i][j]=A[i][j]+min(dp[i-1][0],dp[i-1][1]);
-            }
-        }
+   for(int i=1;i<n;i++){
+                dp[i][0]=A[i][0]+min(dp[i-1][1],dp[i-1][2]);
+                dp[i][1]=A[i][1]+min(dp[i-1][0],dp[i-1][2]);
+                 dp[i][2]=A[i][2]+min(dp[i-1][0],dp[i-1][1]);
+        
     }
     int k=min(dp[n-1][0],dp[n-1][1]);
     return min(k,dp[n-1][2]);

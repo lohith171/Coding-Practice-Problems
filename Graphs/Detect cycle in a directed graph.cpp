4e81a1b24de5db5a1eb9d8ddbd40adebcb86a935
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-07-08 22:07:18 +0530

    Update Detect cycle in a directed graph.cpp

--- Graphs/Detect cycle in a directed graph.cpp
+++ Graphs/Detect cycle in a directed graph.cpp
@@ -1,31 +1,29 @@
-//https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1#
+//https://www.interviewbit.com/old/problems/cycle-in-directed-graph/
 
-class Solution
-{
-    public:
-	//Function to detect cycle in a directed graph.
-	bool solve(int val,vector<int> adj[],vector<bool>& visi,vector<bool>& stk){
-	    visi[val]=1;
-	    stk[val]=1;
-	    for(auto x:adj[val]){
-	        if(stk[x])return 1;
-	        if(!visi[x]){
-	            if(solve(x,adj,visi,stk))return 1;
-	        }
-	    }
-	    stk[val]=0;
-	    return 0;
-	}
-	bool isCyclic(int V, vector<int> adj[]) 
-	{
-	   	// code here
-	   	vector<bool>visi(V,0);
-	   	vector<bool>stk(V,0);
-	   	for(int i=0;i<V;i++){
-	   	    if(!visi[i]){
-	   	        if(solve(i,adj,visi,stk))return 1;
-	   	    }
-	   	}
-	   	return 0;
-	}
-};
+bool issolve(int val,vector<vector<int>>& adj,vector<bool>& visi,vector<bool>& stk){
+    visi[val]=1;
+    stk[val]=1;
+    for(auto x:adj[val]){
+        if(stk[x])return 1;
+        if(!visi[x]){
+            if(issolve(x,adj,visi,stk))return 1;
+        }
+    }
+    stk[val]=0;
+    return 0;
+}
+
+
+
+int Solution::solve(int A, vector<vector<int> > &B) {
+    vector<vector<int>>adj(A+1);
+    for(int i=0;i<B.size();i++){
+        adj[B[i][0]].push_back(B[i][1]);
+    }
+    vector<bool>visi(A+1,0);
+    vector<bool>stk(A+1,0);
+    for(int i=1;i<=A;i++){
+        if(issolve(i,adj,visi,stk))return 1;
+    }
+    return 0;
+}

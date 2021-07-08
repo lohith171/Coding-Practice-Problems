d2c4a19a5a4c54bfed50859c72f8076b2e43a269
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-07-08 21:57:19 +0530

    Update Detect cycle in a undirected graph.cpp

--- Graphs/Detect cycle in a undirected graph.cpp
+++ Graphs/Detect cycle in a undirected graph.cpp
@@ -1,28 +1,29 @@
-//https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1/
+//https://www.interviewbit.com/old/problems/cycle-in-undirected-graph/
 
-class Solution 
-{
-    public:
-    //Function to detect cycle in an undirected graph.
-    bool solve(int val,int parent,vector<int>adj[],vector<bool>& visi){
-        visi[val]=1;
-        for(auto x:adj[val]){
-            if(!visi[x]){
-                if(solve(x,val,adj,visi))return 1;
-            }else if(x!=parent)return 1;
-
+bool issolve(int val,int parent,vector<int> adj[],vector<bool>& visi){
+    visi[val]=1;
+    for(auto x:adj[val]){
+        if(!visi[x]){
+            if(issolve(x,val,adj,visi))return 1;
+        }else{
+            if(x!=parent)return 1;
         }
-        return 0;
     }
-	bool isCycle(int V, vector<int>adj[])
-	{
-	    // Code here
-	    vector<bool>visi(V,0);
-	    for(int i=0;i<V;i++){
-	        if(!visi[i]){
-	            if(solve(i,-1,adj,visi))return 1;
-	        }
-	    }
-	    return 0;
-	}
-};
+    return 0;
+}
+
+
+int Solution::solve(int A, vector<vector<int> > &B) {
+    vector<int> adj[A+1];
+    for(int i=0;i<B.size();i++){
+        adj[B[i][0]].push_back(B[i][1]);
+        adj[B[i][1]].push_back(B[i][0]);
+    }
+    vector<bool>visi(A+1,0);
+    for(int i=1;i<=A;i++){
+        if(!visi[i]){
+            if(issolve(i,-1,adj,visi))return 1;
+        }
+    }
+    return 0;
+}

0f72176d0a2c37f45a046870765274ae4ed59d33
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-07-08 11:55:50 +0530

    Create DFS traversal of a graph.cpp

---
+++ Graphs/DFS traversal of a graph.cpp
@@ -0,0 +1,25 @@
+//https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1#
+
+class Solution 
+{
+    public:
+	//Function to return a list containing the DFS traversal of the graph.
+	void dfs(int val,vector<int> adj[],vector<bool>& visi,vector<int>& res){
+	    if(visi[val]==1)return;
+	        res.push_back(val);
+	        visi[val]=1;
+	         for(auto x:adj[val]){
+	            dfs(x,adj,visi,res);
+	    }
+	    
+	   
+	}
+	vector<int>dfsOfGraph(int V, vector<int> adj[])
+	{
+	    // Code here
+	    vector<bool>visi(V,false);
+	    vector<int>res;
+	    dfs(0,adj,visi,res);
+	    return res;
+	}
+};

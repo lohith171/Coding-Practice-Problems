06317c98e2c7b22d2cf54a3ff6d4603a536e7afc
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-07-22 22:11:14 +0530

    Update Combination sum.cpp

--- Recursion and Backtracking/Combination sum.cpp
+++ Recursion and Backtracking/Combination sum.cpp
@@ -3,16 +3,18 @@
 class Solution {
 public:
     void solve(int ind,int sum,vector<int>v,vector<int>& nums,vector<vector<int>>& res){
-        if(ind==nums.size())return;
+        if(ind==nums.size()){
         if(sum==0){
             res.push_back(v);
-            return;
+        }
+          return;
         }
         if(nums[ind]<=sum){
-            solve(ind+1,sum,v,nums,res);
             v.push_back(nums[ind]);
             solve(ind,sum-nums[ind],v,nums,res);
-        }else return;
+            v.pop_back();
+        }
+         solve(ind+1,sum,v,nums,res);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>res;

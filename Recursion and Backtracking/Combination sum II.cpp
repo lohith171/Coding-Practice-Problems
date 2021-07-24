790602befe13eb207e25b3dc6ecc636018d89f38
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-07-24 17:28:05 +0530

    Update Combination sum II.cpp

--- Recursion and Backtracking/Combination sum II.cpp
+++ Recursion and Backtracking/Combination sum II.cpp
@@ -1,24 +1,40 @@
-//https://leetcode.com/problems/combination-sum-ii/solution/
+//https://www.interviewbit.com/problems/combination-sum-ii/
 
-class Solution {
-public:
-    void solve(int ind,int sum,vector<int>v,vector<int>& nums,vector<vector<int>>& res){
-            if(sum==0)
-                res.push_back(v);
-       for(int i=ind;i<nums.size();i++){
-           if(i!=ind && nums[i-1]==nums[i])continue;
-           if(nums[i]<=sum){
-               v.push_back(nums[i]);
-               solve(i+1,sum-nums[i],v,nums,res);
-               v.pop_back();
-           }
-       }
+/*Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+Each number in C may only be used once in the combination.
+
+Note:
+
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.*/
+
+void solve(int ind,int sum,vector<int>& A,vector<int>v,vector<vector<int>>& res){
+    if(sum==0){
+        res.push_back(v);
+        return;
     }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>res;
-        vector<int>v;
-        sort(candidates.begin(),candidates.end());
-        solve(0,target,v,candidates,res);
-        return res;
+    for(int i=ind;i<A.size();i++){
+        if(i!=ind && A[i-1]==A[i])continue;
+        if(A[i]<=sum){
+            v.push_back(A[i]);
+            solve(i+1,sum-A[i],A,v,res);
+            v.pop_back();
+        }
     }
-};
+}
+
+
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    vector<vector<int>>res;
+    sort(A.begin(),A.end());
+    vector<int>v;
+    solve(0,B,A,v,res);
+    return res;
+
+}

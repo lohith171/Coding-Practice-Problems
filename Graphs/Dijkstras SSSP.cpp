774a70de4c27c03a7c74b08c3f665f1b10688f49
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-07-28 12:38:22 +0530

    Update Dijkstras SSSP.cpp

--- Graphs/Dijkstras SSSP.cpp
+++ Graphs/Dijkstras SSSP.cpp
@@ -12,23 +12,24 @@ class Solution
         vector<int>dist(V,INT_MAX);
         vector<bool>visi(V,0);
         dist[S]=0;
-        visi[S]=1;
         priority_queue<pr,vector<pr>,greater<pr>>pq;
         pq.push({0,S});
         while(!pq.empty()){
-            int d=pq.top().first;
             int v=pq.top().second;
             pq.pop();
+            if(!visi[v]){
             for(auto x:adj[v]){
                 int a=x[0],b=x[1];
-                if(!visi[a]){
+            
                     if(dist[v]+b<dist[a]){
                         dist[a]=dist[v]+b;
                         pq.push({dist[a],a});
                     }
-                }
+            }
+            visi[v]=1;
             }
         }
         return dist;
     }
 };
+

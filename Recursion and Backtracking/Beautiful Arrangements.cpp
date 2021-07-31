b13c13f12efd9836c3dcb6a56a15a6ffadc373de
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-07-31 13:45:12 +0530

    Update Beautiful Arrangements.cpp

--- Recursion and Backtracking/Beautiful Arrangements.cpp
+++ Recursion and Backtracking/Beautiful Arrangements.cpp
@@ -7,6 +7,7 @@ i is divisible by perm[i].
 Given an integer n, return the number of the beautiful arrangements that you can construct.
 */
 
+//Using array
 class Solution {
 public:
     int res=0;
@@ -29,6 +30,32 @@ public:
             nums[i]=i+1;
         }
         solve(1,(1<<n)-1,nums);
+        return res;
+        
+    }
+};
+
+//Not using array
+
+class Solution {
+public:
+    int res=0,k;
+    void solve(int ind,int mask){
+        if(ind==k+1){
+            res++;
+            return;
+        }
+        for(int i=0;i<k;i++){
+            if(mask & 1<<i){
+                if((ind)%(i+1)==0 || (i+1)%(ind)==0){
+                    solve(ind+1,mask^1<<i);
+                }
+            }
+        }
+    }
+    int countArrangement(int n) {
+        k=n;
+        solve(1,(1<<n)-1);
         return res;
         
     }

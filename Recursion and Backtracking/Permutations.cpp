ae841ab5068464c3976759645c5fc667758ac0d8
lohith171 <82638777+lohith171@users.noreply.github.com>
2021-07-31 12:11:21 +0530

    Update Permutations.cpp

--- Recursion and Backtracking/Permutations.cpp
+++ Recursion and Backtracking/Permutations.cpp
@@ -2,18 +2,55 @@
 
 //Given a collection of numbers, return all possible permutations.
 
-vector<vector<int> > Solution::permute(vector<int> &A) {
-    vector<vector<int>>res;
-    if(A.size()==1)return {A};
+//using bactracking
+
+void solve(vector<int>& A,vector<int>v,vector<bool>& vis,vector<vector<int>>& res){
+    if(v.size()==A.size()){
+        res.push_back(v);
+        return;
+    }
     for(int i=0;i<A.size();i++){
-        vector<int>temp=A;
-        temp.erase(temp.begin()+i);
-        vector<vector<int>>tres=permute(temp);
-        for(int j=0;j<tres.size();j++){
-            vector<int>a={A[i]};
-            a.insert(a.end(),tres[j].begin(),tres[j].end());
-            res.push_back(a);
+        if(!vis[i]){
+            vis[i]=1;
+            v.push_back(A[i]);
+            solve(A,v,vis,res);
+            v.pop_back();
+            vis[i]=0;
         }
     }
-    return res;
+}
+
+vector<vector<int> > Solution::permute(vector<int> &A) {
+   int n=A.size();
+   vector<bool>vis(n,0);
+   vector<int>v;
+   vector<vector<int>>res;
+   solve(A,v,vis,res);
+   return res;
+}
+
+
+//Using Bitmasking
+
+void solve(vector<int>& A,vector<int>v,int mask,vector<vector<int>>& res){
+    if(v.size()==A.size()){
+        res.push_back(v);
+        return;
+    }
+    for(int i=0;i<A.size();i++){
+       if(mask & 1<<i){
+           v.push_back(A[i]);
+           solve(A,v,mask^(1<<i),res);
+           v.pop_back();
+       }
+    }
+}
+
+vector<vector<int> > Solution::permute(vector<int> &A) {
+   int n=A.size();
+   
+   vector<int>v;
+   vector<vector<int>>res;
+   solve(A,v,(1<<n)-1,res);
+   return res;
 }
